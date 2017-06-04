38b0c0244b9f07226306d21d58cebf4df120cd90
Thomas Maier <tmaier@cern.ch>
2017-06-04 23:50:20 +0200

    updated and tested

--- tools/jobhandler.py
+++ tools/jobhandler.py
@@ -1,9 +1,10 @@
 
+from __future__ import print_function
 import os
 import subprocess
 import time
 from enum import Enum
-import operator
+from sys import stdout
 
 
 class Status(Enum):
@@ -14,6 +15,7 @@ class Status(Enum):
   Failed = 4
   Cancelled = 5
 
+## TODO: Make more stuff optional and add more options (clusters, qos)
 def slurm_submit(job_name, log_name, partition, run_script):
   submit_string = subprocess.check_output(['sbatch', '-J', job_name, '-o', log_name, '-p', partition, run_script])
   job_id = int(submit_string.split(' ')[-1].rstrip('\n'))
@@ -27,8 +29,9 @@ def slurm_cancel(job_id):
   cancel_string = 'scancel {}'.format(job_id)
   os.system(cancel_string)
 
+## TODO: retrieve status via sacct (field "State")?
 def slurm_status(job_id):
-  status_string = subprocess.check_output(['squeue', '-j', job_id])
+  status_string = subprocess.check_output(['squeue', '-j', str(job_id)])
   n_lines = status_string.count('\n')
   status = Status.Finished
   if n_lines > 1:
@@ -36,14 +39,17 @@ def slurm_status(job_id):
 
   return status
 
+def slurm_exitcode(job_id):
+  sacct_string = subprocess.check_output(['sacct', '-j', str(job_id), '-P', '-o', 'ExitCode'])
+
+  return sacct_string.split('\n')[1]
+
 
 class Job:
 
   ## Should know about all information that it needs to run a single slurm job
   ## Checks itself if it was successful (by whatever means that are defined)
   ## Is able to submit as slurm job or run locally
-  ## TODO: make every job store its slurm job id (via return from sbatch) and use that to directly access it, use job names for other means?
-  ## TODO: need central def file with enums
 
   def __init__(self, name, run_script, log_file, partition, success_func = None, tags = None, parent_tags = None):
     self._name = name
@@ -57,10 +63,7 @@ class Job:
     self._parent_tags = set()
     if parent_tags is not None: self.add_tags(parent_tags, True)
 
-    ## Default: look at return code of slurm jobs?
-    def dummy():
-      return True
-    self._success_func = dummy
+    self._success_func = None
     if success_func is not None: self._success_func = success_func
 
   def add_tag(self, tag, is_parent = False):
@@ -77,8 +80,9 @@ class Job:
       self.add_tag(tags, is_parent)
 
   def submit(self):
-    self._job_id = slurm_submit(self._name, self._log_file, self._partition, self._run_script)
-    self._status = Status.Running
+    if self._status == Status.Configured:
+      self._job_id = slurm_submit(self._name, self._log_file, self._partition, self._run_script)
+      self._status = Status.Running
 
   def cancel(self):
     if self._status != Status.Running:
@@ -91,19 +95,21 @@ class Job:
     if self._status == Status.Running:
       self._status = slurm_status(self._job_id)
     if self._status == Status.Finished:
-      if self._success_func():
-        self._status == Status.Success
+      if self._is_success():
+        self._status = Status.Success
       else:
-        self._status == Status.Failed
+        self._status = Status.Failed
         
     return self._status
 
-  # def get_success(self):
-  #   success = False
-  #   if self._status == Status.Finished:
-  #     success = self._success_func()
-      
-  #   return success
+  def _is_success(self):
+    success = False
+    if self._success_func is None:
+      success = (slurm_exitcode(self._job_id) == '0:0')
+    else:
+      success = self._success_func()
+
+    return success
 
   def get_tags(self):
     return self._tags
@@ -120,14 +126,13 @@ class JobHandler:
   ## Generates Jobs according to configuration
   ## Allow for arbitrary combination of slurm jobs and local (multiprocessing) jobs
   ## TODO: Currently not really working with jobs without any args
-  ## TODO: Rather make the job config adding a function (add job by job) and the in one swoop functionality optional
   ## TODO: Make default setting of stuff like partition and make optional to also define on job to job basis
+  ## TODO: Local job submission (with multiprocessing or subprocess.Popen)
 
-  def __init__(self, name = None, partition = None):
-    self._name = 'hans'
-    if name: self._name = name
-    self._script_folder = 'scripts_'+self._name+'/'
-    self._log_folder = 'logs_'+self._name+'/'
+  def __init__(self, name = 'hans', partition = None):
+    self._name = name
+    self._script_folder = self._name+'/scripts/'
+    self._log_folder = self._name+'/logs/'
     self._jobs = []
     self._tagged_jobs = {}
     self._job_counter = set()
@@ -137,9 +142,9 @@ class JobHandler:
 
   def reset(self):
     if os.path.isdir(self._script_folder): os.system('rm -r '+self._script_folder)
-    os.mkdir(self._script_folder)
+    os.makedirs(self._script_folder)
     if os.path.isdir(self._log_folder): os.system('rm -r '+self._log_folder)
-    os.mkdir(self._log_folder)
+    os.makedirs(self._log_folder)
 
   ## TODO: Make this a generator instead
   def get_jobs(self, tags = None):
@@ -164,58 +169,46 @@ class JobHandler:
               success_func = success_func, tags = tags, parent_tags = parent_tags)
     self._jobs.append(job)
     if tags is not None:
-      for tag in tags:
-        if tag not in self._tagged_jobs: self._tagged_jobs[tag] = []
-        self._tagged_jobs[tag].append(job)
-    
-    # job_name = self._get_job_name(group)
-    # job = Job(job_name, run_script_name, log_name, job_partition)
-    # if job_name not in self._jobs: self._jobs[job_name] = []
-    # self._jobs[job_name].append(job)
-
-  def add_jobs(self, n_jobs, run_script, run_args = None, partition = None):
-    n_args = run_script.count('{}')
-    if n_args > 0 and run_args is None:
-      print ('You have to provide arguments to be used by the job')
-      raise
-    n_args_provided = len(run_args)
-    if n_args > 1 and n_args_provided != n_args:
-      print ('Job requires '+str(n_args)+' separate arguments, '+str(n_args_provided)+' were provided')
-      raise
-    run_args_resolved = []
-    if n_args == 1:
-      run_args = [run_args]
-
-    if run_args is None: return 0
-        
-    for arg in run_args:
-      if isinstance(arg, list) or isinstance(arg, tuple):
-        len_arg = len(arg)
-        if len_arg != n_jobs:
-          print ('Length of argument list is '+str(len_arg)+', while '+str(n_jobs)+' are to be submitted')
-          raise
-        run_args_resolved.append(arg)
+      if isinstance(tags, list) or isinstance(tags, tuple):
+        for tag in tags:
+          if tag not in self._tagged_jobs: self._tagged_jobs[tag] = []
+          self._tagged_jobs[tag].append(job)
       else:
-        arg_list = n_jobs * [arg]
-        run_args_resolved.append(arg)
+        if tags not in self._tagged_jobs: self._tagged_jobs[tags] = []
+        self._tagged_jobs[tags].append(job)
 
-    job_partition = self._partition
-    if partition: job_partition = partition
+  # def add_jobs(self, n_jobs, run_script, run_args = None, partition = None):
+  #   n_args = run_script.count('{}')
+  #   if n_args > 0 and run_args is None:
+  #     print ('You have to provide arguments to be used by the job')
+  #     raise
+  #   n_args_provided = len(run_args)
+  #   if n_args > 1 and n_args_provided != n_args:
+  #     print ('Job requires '+str(n_args)+' separate arguments, '+str(n_args_provided)+' were provided')
+  #     raise
+  #   run_args_resolved = []
+  #   if n_args == 1:
+  #     run_args = [run_args]
 
-    for run_arg in zip(*run_args_resolved):
-      job_run_script = run_script.format(*run_arg)
-      self.add_job(job_run_script, job_partition)
+  #   if run_args is None: return 0
+        
+  #   for arg in run_args:
+  #     if isinstance(arg, list) or isinstance(arg, tuple):
+  #       len_arg = len(arg)
+  #       if len_arg != n_jobs:
+  #         print ('Length of argument list is '+str(len_arg)+', while '+str(n_jobs)+' are to be submitted')
+  #         raise
+  #       run_args_resolved.append(arg)
+  #     else:
+  #       arg_list = n_jobs * [arg]
+  #       run_args_resolved.append(arg)
 
-  # def setup_jobs(self):
-  #   self.reset()
-  #   for i, run_arg in enumerate(zip(*self._run_args)):
-  #     name = self._name+'_'+str(i)
-  #     run_script = self._run_script.format(*run_arg)
-  #     run_script_name = self._write_script(run_script, name)
-  #     log_name = self._log_folder+name
-  #     job = Job(name, run_script_name, log_name, self._partition)
-  #     self._jobs[name] = job
-  #     self._job_ids.add(i)
+  #   job_partition = self._partition
+  #   if partition: job_partition = partition
+
+  #   for run_arg in zip(*run_args_resolved):
+  #     job_run_script = run_script.format(*run_arg)
+  #     self.add_job(job_run_script, job_partition)
 
   def _write_script(self, run_script, name):
     out_file_name = self._script_folder+name
@@ -239,60 +232,38 @@ class JobHandler:
     
     return True
 
-  def run_jobs(self):
-    running = True
+  def run_jobs(self, intervall = 5):
     n_all = len(self._jobs)
+    running = True
     while running:
       self.submit_jobs()
-      time.sleep(10)
       status_dict = self._get_jobs_status()
       n_success = status_dict[Status.Success]
-      if n_success == n_all: running = False
+      n_failed = status_dict[Status.Failed]
+      print_string = 'Jobs success/fail/all: ('+str(n_success)+'/'+str(n_failed)+'/'+str(n_all)+')'
+      stdout.write('\r'+print_string)
+      stdout.flush()
+      time.sleep(intervall)
+      if (n_success+n_failed) == n_all: running = False
+    stdout.write('\n')
 
   def submit_jobs(self, tags = None):
     for job in self.get_jobs(tags):
       if not self._check_job_readiness(job): continue
       job.submit()
-    
-    # if groups is None:
-    #   for job_list in self._jobs.values():
-    #     for job in job_list:
-    #       job.submit()
-    # else:
-    #   job_name = self._get_job_name(group)
-    #   for job in self._jobs[job_name]:
-    #     job.submit()
 
   def cancel_jobs(self, tags = None):
     for job in self.get_jobs(tags):
       job.cancel()
 
-  # def check_jobs_status(self):
-  #   n_finished = 0
-  #   n_all = len(self._jobs)
-  #   for job in self._jobs.values():
-  #     if job.get_status() != 'finished': continue
-  #     n_finished += 1
-  #   print ('Jobs finished/all:', '('+str(n_finished)+'/'+str(n_all)+')')
-
   ## TODO: must be rather check_jobs_status with some decision making if jobs failed (retry logic, maybe job can automatically gather on which machine it was running on)
   def _get_jobs_status(self):
-    status_dict = {}
+    status_dict = {Status.Configured: 0, Status.Running: 0, Status.Finished: 0, Status.Success: 0, Status.Failed: 0, Status.Cancelled: 0}
     for job in self._jobs:
       status = job.get_status()
-      if not status in status_dict: status_dict[status] = 0
       status_dict[status] += 1
 
     return status_dict
-
-  # status_tuple = sorted(status_dict.items(), key = operator.itemgetter(1), reverse = False)
-
-  # JOB_STATUS = {'configured': 0,
-  #             'running': 1,
-  #             'finished': 2,
-  #             'success': 3,
-  #             'failed': 4,
-  #             'cancelled': 5}
 
   ## TODO: modify so that it allows to specify tags
   def check_status(self):
@@ -300,17 +271,6 @@ class JobHandler:
     n_all = str(len(self._jobs))
     for status, n_jobs in status_dict.items():
       print (status.name+':', '('+str(n_jobs)+'/'+n_all+')')
-
-  # def check_status(self):
-  #   n_success = 0
-  #   n_finished = 0
-  #   n_all = len(self._jobs)
-  #   for job in self._jobs:
-  #     if job.get_status() != Status.Finished: continue
-  #     n_finished += 1
-  #     if not job.get_success(): continue
-  #     n_success += 1
-  #   print ('Jobs successful/finished/all:', '('+str(n_success)+'/'+str(n_finished)+'/'+str(n_all)+')')
 
   @staticmethod
   def _has_tag(job, tag):
